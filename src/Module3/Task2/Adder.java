0395ede3a49dae213188f65829e6501c91e55d81
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-02-22 17:24:12 +0200

    Revision (n+1)

---
+++ src/Module3/Task2/Adder.java
@@ -0,0 +1,10 @@
+package Module3.Task2;
+
+/**
+ * Created by George on 22/02/2017.
+ */
+public class Adder extends Arithmetic {
+        boolean check(Integer a, Integer b){
+            return  a>=b;
+        }
+}
---
+++ src/Module3/Task2/Arithmetic.java
@@ -0,0 +1,10 @@
+package Module3.Task2;
+
+/**
+ * Created by George on 22/02/2017.
+ */
+public class Arithmetic {
+    int add(Integer a, Integer b){
+        return a+b;
+    }
+}
