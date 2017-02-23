ec4ebec1afcaf7787172d6450f4fc270fb41707a
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-02-23 17:45:15 +0200

    Output class added

---
+++ src/Module3/Task4/Output.java
@@ -0,0 +1,15 @@
+package Module3.Task4;
+
+/**
+ * Created by George on 23/02/2017.
+ */
+public class Output {
+    public static final String SPLITTER = "_____________________________";
+    public static final String SUCCESS = " OPERATION SUCCESSFUL.";
+    public static final String CURRENT_BALANCE  = "THE CURRENT BALANCE IS: ";
+    public static final String WITHDRAWAL_IMPOSSIBLE = "WITHDRAWAL IS IMPOSSIBLE";
+    public static final String BALANCE_AFTER_TRANSACTION = "YOUR BALANCE AFTER THE LAST TRANSACTION IS";
+    public static final String FEE = "FEE TAKEN: ";
+    public static final String WITHDRAWAL_ATTEMPT = "ATTEMPTING YO WITHDRAW: ";
+    public static final String EARNED_SALARY = "YOU EARNED: ";
+}
--- src/Module3/Task4/User.java
+++ src/Module3/Task4/User.java
@@ -4,9 +4,46 @@ package Module3.Task4;
  * Created by George on 23/02/2017.
  */
 public class User {
-    String name;
-    int monthsOfEmployment;
-    String companyName;
-    int salary;
-    String currency;
+    private String name;
+    private int balance;
+    private int monthsOfEmployment;
+    private String companyName;
+    private int salary;
+    private String currency;
+    private final int fee1 = 5;
+    private final int fee2 = 10;
+    private final boolean IS_LOG_ENABLED = true;
+
+    public User(String name, int balance, int monthsOfEmployment,
+                String companyName, int salary, String currency) {
+        this.name = name;
+        this.balance = balance;
+        this.monthsOfEmployment = monthsOfEmployment;
+        this.companyName = companyName;
+        this.salary = salary;
+        this.currency = currency;
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Phillip Morris", 20000, 48, "Phillip Morris Intl", 2000, "EUR");
+        user1.withdraw(5000);
+        user1.withdraw(7000);
+        user1.withdraw(500);
+    }
+
+    public void withdraw(int sum){
+        int balanceAfterWithdrawalWithFee = balance - sum - (int)(sum*(sum<1000? fee1:fee2)/100);
+        boolean isWithdrawalPermitted = balanceAfterWithdrawalWithFee >=0;
+
+        showLog(.CURRENT_BALANCE );
+    }
+
+    public void showLog(String information){
+        if (!IS_LOG_ENABLED){
+            return;
+        }
+        System.out.println(information);
+    }
+
+
 }
