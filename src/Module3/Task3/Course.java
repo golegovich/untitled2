a47fc7c163b231696c92eb0de0f72a71077599bd
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-02-23 11:35:16 +0200

    Added the Solution Class and created get/set in the Course Class

--- src/Module3/Task3/Course.java
+++ src/Module3/Task3/Course.java
@@ -24,5 +24,29 @@ public class Course  {
             this.teacherName = teacherName;
     }
 
-
+    //Getters Setters
+    public Date getStartDate(){
+        return startDate;
+    }
+    public void setStartDate(Date startDate){
+        this.startDate = startDate;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getHoursDuration(){
+        return hoursDuration;
+    }
+    public void setHoursDuration(){
+        this.hoursDuration = hoursDuration;
+    }
+    public String getTeacherName(){
+        return  teacherName;
+    }
+    public void setTeacherName(){
+        this.teacherName = teacherName;
+    }
 }
