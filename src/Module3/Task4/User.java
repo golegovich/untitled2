5eaf7de656b2bb4c597d3f0e95e8dc73ccc74d6f
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-02-23 17:16:33 +0200

    created the User class

--- src/Module3/Task4/User.java
+++ src/Module3/Task4/User.java
@@ -4,4 +4,9 @@ package Module3.Task4;
  * Created by George on 23/02/2017.
  */
 public class User {
+    String name;
+    int monthsOfEmployment;
+    String companyName;
+    int salary;
+    String currency;
 }
