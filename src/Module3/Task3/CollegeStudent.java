a73975527659416d8a5bc293dec4efb78f6f26b9
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-02-23 12:02:06 +0200

    THX ALLAH, EVERYTHING GOES FINE

--- src/Module3/Task3/CollegeStudent.java
+++ src/Module3/Task3/CollegeStudent.java
@@ -15,15 +15,32 @@ public class CollegeStudent extends Student {
     public CollegeStudent(String lastName, Course[] coursesTaken) {
         super(lastName, coursesTaken);
     }
-    public CollegeStudent(String firstName, String lastName, int group, Course [] coursesTaken
+    public CollegeStudent(String firstName, String lastName, int group, Course [] coursesTaken,
     int age, String collegeName, int rating, long id){
         this(firstName, lastName, group);
-        this.setCouresTaken(coursesTaken);
+        this.setCoursesTaken(coursesTaken);
         this.setAge(age);
         this.collegeName = collegeName;
         this.rating = rating;
         this.id = id;
     }
-
-
+    //Get-set
+    public String getCollegeName(){
+        return collegeName;
+    }
+    public void setCollegeName(String collegeName){
+        this.collegeName = collegeName;
+    }
+    public int getRating(){
+        return rating;
+    }
+    public void setRating(int rating){
+        this.rating = rating;
+    }
+    public long getId(){
+        return id;
+    }
+    public void setId(long id){
+        this.id = id;
+    }
 }
