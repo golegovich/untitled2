41c38679753964d477caf8341a1051029a92a94e
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-03-06 15:44:15 +0200

    Added a constructor to the User Added the logic to the USBank class

--- src/Module4/USBank.java
+++ src/Module4/USBank.java
@@ -1,7 +1,61 @@
 package Module4;
 
+import java.util.*;
+
 /**
  * Created by George on 06/03/2017.
  */
 public abstract class USBank extends Bank {
+
+    Currency currency = Currency.USD;
+
+    @Override
+    int getLimitofWithdrawal() {
+        int limit = 0;
+        if (currency.equals(Currency.USD)) {
+            limit = 1000;
+        }
+        if (currency.equals(Currency.EUR)) {
+            limit = 1200;
+        }
+        return limit;
+
+
+    }
+
+    @Override
+    int getLimitOfFunding() {
+        int limit = 0;
+        if (currency.equals(Currency.USD)) {
+            limit = Integer.MAX_VALUE;
+        }
+        if (currency.equals(Currency.EUR)) {
+            limit = 10000;
+        }
+        return limit;
+    }
+
+    @Override
+    int getMonthlyRate() {
+        int rate = 0;
+        if (currency.equals(Currency.USD)){
+            rate = 1;
+        }
+        if (currency.equals(Currency.EUR)){
+            rate = 2;
+        }
+        return rate;
+    }
+
+    @Override
+    int getComission(int sum) {
+        int comission = 0;
+        if (currency.equals(Currency.USD)){
+            comission = sum > 1000 ? 7:5;
+        }
+        if (currency.equals(Currency.EUR)){
+            comission = sum >1000 ? 8:6;
+        }
+        return comission;
+    }
 }
--- src/Module4/User.java
+++ src/Module4/User.java
@@ -12,4 +12,14 @@ public class User {
     String companyName;
     int salary;
     Bank bank;
+
+    public User(long id, String name, double balance, int monthsOfEmployment, String companyName, int salary, Bank bank) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+        this.monthsOfEmployment = monthsOfEmployment;
+        this.companyName = companyName;
+        this.salary = salary;
+        this.bank = bank;
+    }
 }
