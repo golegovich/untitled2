1deb319c02ac16709885e47a350fa926cfb85469
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-03-06 15:10:50 +0200

    Started doing the 4th module, created the Bank class

---
+++ src/Module4/Bank.java
@@ -0,0 +1,82 @@
+package Module4;
+
+import java.util.Currency;
+
+/**
+ * Created by George on 06/03/2017.
+ */
+public abstract class Bank {
+
+    long id;
+    String bankCountry;
+    Currency currency;
+    int numberOfEmployees;
+    double avrSalaryOfEmployee;
+    long rating;
+    long totalCapital;
+
+    abstract int getLimitofWithdrawal();
+    abstract int getLimitOfFunding();
+    abstract int getMonthlyRate();
+    abstract int getComission(int sum);
+    abstract double moneyPaidMonthlyForSalary();
+
+    //get-set
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBankCountry() {
+        return bankCountry;
+    }
+
+    public void setBankCountry(String bankCountry) {
+        this.bankCountry = bankCountry;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public int getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(int numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public double getAvrSalaryOfEmployee() {
+        return avrSalaryOfEmployee;
+    }
+
+    public void setAvrSalaryOfEmployee(double avrSalaryOfEmployee) {
+        this.avrSalaryOfEmployee = avrSalaryOfEmployee;
+    }
+
+    public long getRating() {
+        return rating;
+    }
+
+    public void setRating(long rating) {
+        this.rating = rating;
+    }
+
+    public long getTotalCapital() {
+        return totalCapital;
+    }
+
+    public void setTotalCapital(long totalCapital) {
+        this.totalCapital = totalCapital;
+    }
+}
