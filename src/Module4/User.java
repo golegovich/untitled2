d27c5c50c269fb7b26fabf82cc2637caa3a2b2c3
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-03-06 15:25:20 +0200

    Created the User class

---
+++ src/Module4/User.java
@@ -0,0 +1,15 @@
+package Module4;
+
+/**
+ * Created by George on 06/03/2017.
+ */
+public class User {
+
+    long id;
+    String name;
+    double balance;
+    int monthsOfEmployment;
+    String companyName;
+    int salary;
+    Bank bank;
+}
