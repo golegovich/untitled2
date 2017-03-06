e8f24f386b67b9bf4267339b9893dc4e4218e136
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-03-06 15:59:20 +0200

    Done everything that is required in part 4 of the task 4

--- src/Module4/Bank.java
+++ src/Module4/Bank.java
@@ -5,7 +5,7 @@ import java.util.Currency;
 /**
  * Created by George on 06/03/2017.
  */
-public abstract class Bank {
+public abstract class Bank implements BankSystem {
 
     long id;
     String bankCountry;
--- src/Module4/User.java
+++ src/Module4/User.java
@@ -22,4 +22,65 @@ public class User {
         this.salary = salary;
         this.bank = bank;
     }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public int getMonthsOfEmployment() {
+        return monthsOfEmployment;
+    }
+
+    public void setMonthsOfEmployment(int monthsOfEmployment) {
+        this.monthsOfEmployment = monthsOfEmployment;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
 }
