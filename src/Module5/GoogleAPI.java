00320f21842cc6b865d293f6a2de2646b630c744
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-03-15 16:25:38 +0200

    Added three implementations of the API interface

---
+++ src/Module5/GoogleAPI.java
@@ -0,0 +1,11 @@
+package Module5;
+
+/**
+ * Created by George on 15/03/2017.
+ */
+public class GoogleAPI implements API{
+    @Override
+    public Room[] findRooms(int price, int persons, String city, String hotel) {
+        return new Room[0];
+    }
+}
---
+++ src/Module5/TripAdvisorAPI.java
@@ -0,0 +1,11 @@
+package Module5;
+
+/**
+ * Created by George on 15/03/2017.
+ */
+public class TripAdvisorAPI implements API {
+    @Override
+    public Room[] findRooms(int price, int persons, String city, String hotel) {
+        return new Room[0];
+    }
+}
