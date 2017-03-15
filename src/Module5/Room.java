78eba79c7f48f7896e091912c1ec3665b746c73e
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-03-15 16:18:53 +0200

    Overridden equals() and hashCode()

--- src/Module5/Room.java
+++ src/Module5/Room.java
@@ -77,4 +77,14 @@ public class Room {
     public void setCityName(String cityName) {
         this.cityName = cityName;
     }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
