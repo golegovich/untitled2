ea558fc5c45ad3a8905fc47069d2e99dcc725c0a
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-03-15 16:32:50 +0200

    Created the Controller interface

---
+++ src/Module5/Controller.java
@@ -0,0 +1,18 @@
+package Module5;
+
+import com.sun.javafx.binding.StringFormatter;
+
+/**
+ * Created by George on 15/03/2017.
+ */
+public class Controller {
+    static API apis[] = new API[3];
+
+    Room[] requestrooms(int price, int persons, String city, String hotel){
+        return new Room[0];
+    }
+
+    Room[] check (API api1, API api2){
+        return new Room[0];
+    }
+}
