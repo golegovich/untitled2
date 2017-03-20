1830c492849c303d7489620f2ad578c0a5926633
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-03-20 21:55:15 +0200

    Major changes done to the Controller class

--- src/Module5/Controller.java
+++ src/Module5/Controller.java
@@ -1,6 +1,5 @@
 package Module5;
 
-import com.sun.javafx.binding.StringFormatter;
 
 /**
  * Created by George on 15/03/2017.
@@ -8,11 +7,60 @@ import com.sun.javafx.binding.StringFormatter;
 public class Controller {
     static API apis[] = new API[3];
 
-    Room[] requestrooms(int price, int persons, String city, String hotel){
-        return new Room[0];
+    public Controller(){
+        apis[0] = new BookingComAPI();
+        apis[1] = new GoogleAPI();
+        apis[2] = new TripAdvisorAPI();
     }
 
-    Room[] check (API api1, API api2){
-        return new Room[0];
+    Room[] requestrooms(int price, int persons, String city, String hotel){
+
+        Room[] roomArray = new Room[20];
+        int count = 0;
+        Room[] rooms;
+
+        for (int i; i<apis.length; i++){
+            rooms = apis[i].findRooms(price, persons, city, hotel);
+            System.arraycopy(rooms, 0, roomArray, count, rooms.length);
+            count +=rooms.length;
+        }
+        return roomArray;
+    }
+
+    public Room[] check (DAO dao1, DAO dao2){
+        Room[] rooms = new Room [0];
+        Room[] rooms1 = dao1.getAll();
+        Room[] rooms2 = dao2.getAll();
+
+        int length1 = rooms1.length;
+        int length2 = rooms2.length;
+        int k = -1;
+
+        for (int i=0; i<length1; i++) {
+            for (int j = 0; j < length2; j++) {
+                if (rooms1[i] != null && rooms[i].equals(rooms2[j]) && j != k) {
+                    k = j;
+                    rooms = addElementToArray(rooms, rooms1[i]);
+                    rooms = addElementToArray(rooms, rooms2[j]);
+                }
+            }
+        }
+        return rooms;
+    }
+    public static void saveToAllDAO(Room room, DAO[] dao){
+        for (int i=0; i<dao.length; i++){
+            dao[i].save(room);
+        }
+    }
+
+    public static void deleteFromAllDAO(Room room, DAO[] dao){
+        for (int i=0; i<dao.length; i++){
+        dao[i].delete(room);
+        }
+    }
+    public static void updateToAllDAO (Room room, DAO[] dao){
+        for (int i=0; i<dao.length; i++){
+            dao[i].update(room);
+        }
     }
 }
