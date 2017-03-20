50016cae85919d93dd680a2f9f5498ace0e0d90b
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-03-20 18:12:06 +0200

    Work in progress on the DAO

--- src/Module5/GoogleDAO.java
+++ src/Module5/GoogleDAO.java
@@ -1,14 +1,26 @@
 package Module5;
 
+import java.time.LocalDate;
+import java.time.Month;
+
 /**
  * Created by George on 20/03/2017.
  */
 public class GoogleDAO implements DAO {
 
+    private static Room[] rooms = new Room [10];
 
+    public  GoogleDAO(){
+
+        rooms[0] = new Room(18, 100, 1, LocalDate.of(2017, Month.JANUARY, 1), "Landmark","London");
+        rooms[1] = new Room(2, 200, 2, LocalDate.of(2017, Month.DECEMBER, 21), "Carrow Road Inn", "Manchester");
+        rooms[2] = new Room(8, 150, 3, LocalDate.of(2017, Month.AUGUST, 6), "Parc de Princess", "Paris");
+        rooms[3] = new Room(25, 500, 2, LocalDate.of(2017, Month.MAY, 31),  "L'Imperialle", "Marseille");
+        rooms[4] = new Room(13, 300, 3, LocalDate.of(2018, Month.MARCH, 17), "Intercontinental", "Kyiv");
+    }
     @Override
     public Room save(Room room) {
-        return null;
+        int count = RoomsInDBQuant(rooms);
     }
 
     @Override
--- src/Module5/Room.java
+++ src/Module5/Room.java
@@ -1,6 +1,6 @@
 package Module5;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 
 /**
@@ -11,7 +11,7 @@ public class Room {
     private long id;
     private int price;
     private int persons;
-    private Date dateAvailableFrom;
+    private LocalDate dateAvailableFrom;
     private String hotelName;
     private String cityName;
 
@@ -20,7 +20,7 @@ public class Room {
 
 
     public Room(long id, int price, int persons,
-                Date dateAvailableFrom, String hotelName,
+                LocalDate dateAvailableFrom, String hotelName,
                 String cityName) {
         this.id = id;
         this.price = price;
@@ -55,11 +55,11 @@ public class Room {
         this.persons = persons;
     }
 
-    public Date getDateAvailableFrom() {
+    public LocalDate getDateAvailableFrom() {
         return dateAvailableFrom;
     }
 
-    public void setDateAvailableFrom(Date dateAvailableFrom) {
+    public void setDateAvailableFrom(LocalDate dateAvailableFrom) {
         this.dateAvailableFrom = dateAvailableFrom;
     }
 
