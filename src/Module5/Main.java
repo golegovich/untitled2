1a74a403bd92b7bce92f8019b717370413c6b7cd
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-03-21 16:59:15 +0200

    Work in progress on Main

--- src/Module5/Main.java
+++ src/Module5/Main.java
@@ -5,6 +5,26 @@ package Module5;
  */
 public class Main {
     public static void main(String[] args) {
-        
+
+        Controller controller = new Controller();
+        DAO[] dao = new DAO[]{new GoogleDAO()};
+
+        System.out.println("\tDB before the changes: ");
+
+    }
+
+    private static void printAllDAO(DAO[] dao) {
+        for (int i = 0; i < dao.length; i++) {
+            System.out.println("\t" + dao[i].getClass());
+            printRoomArray(dao[i].getAll());
+        }
+    }
+
+    private static void printRoomArray(Room[] rooms) {
+        for (Room room : rooms) {
+            if (room != null) {
+                System.out.println(room);
+            }
+        }
     }
 }
