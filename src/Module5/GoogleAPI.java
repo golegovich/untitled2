b21e8a30e5801bbd0b5a48852c2454e8367edc1c
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-03-21 16:48:41 +0200

    Done with API

--- src/Module5/GoogleAPI.java
+++ src/Module5/GoogleAPI.java
@@ -1,24 +1,32 @@
 package Module5;
 
+import java.util.Arrays;
+
 /**
  * Created by George on 15/03/2017.
  */
-public class GoogleAPI implements API{
+public class GoogleAPI implements API {
 
     private DAO dao = new GoogleDAO;
     public Room[] rooms = dao.getAll();
 
 
-
     @Override
     public Room[] findRooms(int price, int persons, String city, String hotel) {
-        Room[] roomSearchArray = new Room [0];
+        Room[] roomSearchArray = new Room[0];
         Room searchingForRoom = new Room(1, 100, 1, null, hotel, city);
 
-        for (int i; i<rooms.length; i++){
-            if (roomSearchArray.equals(rooms[i]) && rooms[i].getHotelName() ==hotel)
+        for (int i = 0; i < rooms.length; i++) {
+            if (searchingForRoom.equals(rooms[i]) && rooms[i].getHotelName() == hotel)
                 roomSearchArray = addElementToArray(roomSearchArray, rooms[i]);
         }
         return roomSearchArray;
+    }
+
+    private Room[] addElementToArray(Room[] roomsArray, Room newRoomsMember){
+
+        Room[] newRoomsArray = Arrays.copyOf(roomsArray, roomsArray.length + 1);
+        newRoomsArray[newRoomsArray.length - 1] = newRoomsMember;
+        return  newRoomsArray;
     }
 }
