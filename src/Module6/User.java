7e98b7394f362118dc4167fdea63ea0372e1223d
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-03-29 14:42:27 +0300

    Created the User class

---
+++ src/Module6/User.java
@@ -0,0 +1,21 @@
+package Module6;
+
+/**
+ * Created by George on 29/03/2017.
+ */
+public final class User {
+
+    private long id;
+    private String firstName, lastName;
+    private int salary;
+    private int balance;
+
+    public User(long id, String firstName, String lastName, int salary, int balance) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.balance = balance;
+    }
+
+}
