e3c74f26f710c46164697349e87a77a351f68301
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-05-16 12:02:37 +0300

    main

--- src/Module7/Offline/Hausafgaben/Main.java
+++ src/Module7/Offline/Hausafgaben/Main.java
@@ -1,5 +1,9 @@
 package Module7.Offline.Hausafgaben;
 
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.List;
+
 /**
  * Created by Georgii Tarasenko
  * on 15/05/2017
@@ -7,6 +11,17 @@ package Module7.Offline.Hausafgaben;
 public class Main {
     public static void main(String[] args) {
         User user1 = new User (1, "John", "Terry", "London", 10000);
+        User user2 = new User (2, "Eden", "Hazard", "Bruxelles", 20000);
+        User user3 = new User (3, "Diego", "Costa", "Sao Paolo", 15000);
+        User user4 = new User (4, "Marcos", "Alonso", "Seville", 9000);
+        User user5 = new User (5, "N'Golo", "Kante", "Marseilles", 17000);
+        User user6 = new User (6, "Mishy", "Batshuayi", "Brugge", 8000);
+        User user7 = new User (7, "Cristiano", "Ronaldo", "Madrid", 50000);
+        User user8 = new User (8, "Sergio", "Ramos", "Valencia", 25000);
+        User user9 = new User (9, "Gareth", "Bale", "Cardiff", 30000);
+        User user10 = new User (10, "Karim", "Benzema", "Algiers", 1000);
+
+        List<Order> orders = new ArrayList<>(10);
 
     }
 }
