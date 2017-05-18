c3c544820146b7721d14e65f6e4ac414bdd5be61
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-05-18 15:46:04 +0300

    Paddling

--- src/Module7/Offline/Hausafgaben/Main.java
+++ src/Module7/Offline/Hausafgaben/Main.java
@@ -9,55 +9,74 @@ import java.util.List;
  */
 public class Main {
     public static void main(String[] args) {
-        User user1 = new User (1, "John", "Terry", "London", 10000);
-        User user2 = new User (2, "Eden", "Hazard", "Bruxelles", 20000);
-        User user3 = new User (3, "Diego", "Costa", "Sao Paolo", 15000);
-        User user4 = new User (4, "Marcos", "Alonso", "Seville", 9000);
-        User user5 = new User (5, "N'Golo", "Kante", "Marseilles", 17000);
-        User user6 = new User (6, "Mishy", "Batshuayi", "Brugge", 8000);
-        User user7 = new User (7, "Cristiano", "Ronaldo", "Madrid", 50000);
-        User user8 = new User (8, "Sergio", "Ramos", "Valencia", 25000);
-        User user9 = new User (9, "Gareth", "Bale", "Cardiff", 30000);
-        User user10 = new User (10, "Karim", "Benzema", "Algiers", 1000);
+        User user1 = new User(1, "John", "Terry", "London", 10000);
+        User user2 = new User(2, "Eden", "Hazard", "Bruxelles", 20000);
+        User user3 = new User(3, "Diego", "Costa", "Sao Paolo", 15000);
+        User user4 = new User(4, "Marcos", "Alonso", "Seville", 9000);
+        User user5 = new User(5, "N'Golo", "Kante", "Marseilles", 17000);
+        User user6 = new User(6, "Mishy", "Batshuayi", "Brugge", 8000);
+        User user7 = new User(7, "Cristiano", "Ronaldo", "Madrid", 50000);
+        User user8 = new User(8, "Sergio", "Ramos", "Valencia", 25000);
+        User user9 = new User(9, "Gareth", "Bale", "Cardiff", 30000);
+        User user10 = new User(10, "Karim", "Benzema", "Algiers", 1000);
 
         List<Order> orders = new ArrayList<>(10);
 
-        Order order1 = new Order (1, 20, Currency.USD, "Mark1", "Gatlng", user1);
-        Order order2 = new Order (2, 10, Currency.UAH, "Mark2", "Browning", user2);
-        Order order3 = new Order (3, 40, Currency.UAH, "Mark3", "Colt", user4);
-        Order order4 = new Order (4, 500, Currency.USD, "Mark4", "Walter", user6);
-        Order order5 = new Order (5, 40, Currency.USD, "Mark3", "Colt", user4);
-        Order order6 = new Order (6, 600, Currency.UAH, "Mark6", "Kingsman", user7);
-        Order order7 = new Order (7, 400, Currency.UAH, "Mark7", "Gatlng", user8);
-        Order order8 = new Order (8, 100, Currency.USD, "Mark8", "Colt", user2);
-        Order order9 = new Order (9, 200, Currency.USD, "Mark9", "Gatlng", user10);
-        Order order10 = new Order (10, 450, Currency.UAH, "Mark10", "Walter", user5);
+        Order order1 = new Order(1, 20, Currency.USD, "Mark1", "Gatlng", user1);
+        Order order2 = new Order(2, 1500, Currency.UAH, "Mark2", "Browning", user2);
+        Order order3 = new Order(3, 1500, Currency.UAH, "Mark3", "Colt", user4);
+        Order order4 = new Order(4, 500, Currency.USD, "Mark4", "Walter", user6);
+        Order order5 = new Order(5, 40, Currency.USD, "Mark3", "Colt", user4);
+        Order order6 = new Order(6, 600, Currency.UAH, "Mark6", "Kingsman", user7);
+        Order order7 = new Order(7, 400, Currency.UAH, "Mark7", "Gatlng", user8);
+        Order order8 = new Order(8, 100, Currency.USD, "Mark8", "Colt", user2);
+        Order order9 = new Order(9, 200, Currency.USD, "Mark9", "Gatlng", user10);
+        Order order10 = new Order(10, 450, Currency.UAH, "Mark10", "Walter", user5);
 
-        orders.addAll(Arrays.asList(order1, order2, order3, order4, order5, order6, order7, order8, order9, order10 ));
+        orders.addAll(Arrays.asList(order1, order2, order3, order4, order5, order6, order7, order8, order9, order10));
         System.out.println("A collection of orders: ");
         orders.forEach(System.out::print);
 
         Collections.sort(orders, Comparator.reverseOrder());
         print("Collection sorted in the reversed order: ", orders);
+
+        Collections.sort(orders, new Comparator<Order>() {
+            @Override
+            public int compare(Order o1, Order o2) {
+                int priceIncreaseEval = ((Integer) (o1.getPrice())).compareTo(o2.getPrice());
+                if (priceIncreaseEval != 0) {
+                    return priceIncreaseEval;
+                }
+                return o1.getUser().getCity().compareTo(o2.getUser().getCity());
+            }
+        });
+        print(" Collection sorted in 'item, name, city' order: ", orders);
+        //deleting duplicates
+        List<Order> uniqueOrder1 = new ArrayList<>(new HashSet<>(orders));
+        print("Duplicates removed from the collection: ", uniqueOrder1);
+        //removing positions with the price less than 1500
+        List<Order> ordersLessThan1500 = new ArrayList<>(orders);
+        for (Iterator<Order> iterator = ordersLessThan1500.iterator(); iterator.hasNext(); ) {
+            if (iterator.next().getPrice() < 1500)
+                iterator.remove();
+        }
+        print("Items less than 1500 removed: ", ordersLessThan1500);
+
+        //dividing list into hryvniyas and dollars
+        List<Order> ordersUAH = new ArrayList<>();
+        List<Order> ordersUSD = new ArrayList<>();
+        for (Order order : orders) {
+            if (order.getCurrency() == Currency.UAH)
+                ordersUAH.add(order);
+            else if (order.getCurrency() == Currency.USD)
+                ordersUSD.add(order);}
+            print("USD orders: ", ordersUSD);
+            print("UAH orders: ", ordersUAH);
+
+
+        //dividing list into multitude of lists on the basis of User' cities
+
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     public static void print(String string, List orders){
         System.out.println(string);
