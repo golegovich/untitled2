88fc43fe94349d69f1879de989371e04d6203307
Georgiy Tarasenko <tar.georgiy@gmail.com>
2017-05-22 18:21:15 +0300

    Tree Set for main created

--- src/Module7/Offline/Hausafgaben/Main.java
+++ src/Module7/Offline/Hausafgaben/Main.java
@@ -12,7 +12,7 @@ import static java.util.Comparator.reverseOrder;
  */
 public class Main {
     public static void main(String[] args) {
-        User user1 = new User(1, "John", "Terry", "London", 10000);
+         User user1 = new User(1, "John", "Terry", "London", 10000);
         User user2 = new User(2, "Eden", "Hazard", "Bruxelles", 20000);
         User user3 = new User(3, "Diego", "Costa", "Sao Paolo", 15000);
         User user4 = new User(4, "Marcos", "Alonso", "Seville", 9000);
