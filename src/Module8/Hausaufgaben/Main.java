9454a5887213351bb78557b8f89df19f2135d54b
George <loginnenuzhen@gmail.com>
2017-06-07 19:07:22 +0300

    Making things right

--- src/Module8/Hausaufgaben/Main.java
+++ src/Module8/Hausaufgaben/Main.java
@@ -1,6 +1,6 @@
 package Module8.Hausaufgaben;
 
-import Module8.Hausaufgaben.Food.Food;
+import Module8.Hausaufgaben.Food.*;
 
 /**
  * Created by Georgii Tarasenko
@@ -10,5 +10,10 @@ import Module8.Hausaufgaben.Food.Food;
 public class Main {
     public static void main(String[] args) {
         InterfaceImpl<Food> interfaceImpl = new InterfaceImpl<>();
-        interfaceImpl.save(new DairyProducts("Mellowed Cheese")
+        interfaceImpl.save(new DairyProducts("Mellowed Cheese", Country.FRANCE, 5), 45.0);
+        interfaceImpl.save(new Bakery("Oat bread", Country.USA, 3), 15.5);
+        interfaceImpl.save(new Berries("Water melon", Country.UKRAINE, 50), 3.7);
+
+        Food beverages = interfaceImpl.save(new Beverages("London Pride", Country.UK, ))
+    }
 }
