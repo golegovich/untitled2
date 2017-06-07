5a92e3ca02f73187b6c52f550b19d914e5ac8184
George <loginnenuzhen@gmail.com>
2017-06-07 21:26:54 +0300

    Done with Main

--- src/Module8/Hausaufgaben/Main.java
+++ src/Module8/Hausaufgaben/Main.java
@@ -2,6 +2,9 @@ package Module8.Hausaufgaben;
 
 import Module8.Hausaufgaben.Food.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Georgii Tarasenko
  * on 07.06.2017.
@@ -14,6 +17,21 @@ public class Main {
         interfaceImpl.save(new Bakery("Oat bread", Country.USA, 3), 15.5);
         interfaceImpl.save(new Berries("Water melon", Country.UKRAINE, 50), 3.7);
 
-        Food beverages = interfaceImpl.save(new Beverages("London Pride", Country.UK, ))
+        Food beverages = interfaceImpl.save(new Beverages("London Pride", Country.UK, 120), 40);
+
+        Groceries buckwheat = new Groceries("Zhmen'ka", Country.UKRAINE, 30);
+        interfaceImpl.save(buckwheat);
+
+        Map<Berries, Double> berriesDb = new HashMap<>();
+        berriesDb.put(new Berries("Victory", Country.BRAZIL, 120), 12.0);
+        interfaceImpl.saveAll(berriesDb);
+
+        System.out.println(interfaceImpl.getProducts());
+        System.out.println(interfaceImpl.getPrice(beverages));
+        System.out.println(interfaceImpl.getPrices());
+        System.out.println(interfaceImpl.get(4));
+        System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
